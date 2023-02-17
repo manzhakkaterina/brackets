78d48bd2c36a04d612207ee610c94e60df981c8c
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-02-17 22:30:50 +0200

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const doub = new Map(bracketsConfig);
+  const mass = []; 
+  for (const symb of str) { 
+    if (doub.get(mass.at(-1)) === symb) {
+      mass.pop(); 
+    } else {
+      mass.push(symb);
+    }
+  }
+  if (mass.length === 0) {
+  return true; 
+  } else {
+    return false;
+  }
 }
+
